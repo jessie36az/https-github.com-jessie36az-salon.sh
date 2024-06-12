d6fd9662e19a732edda171ab3616783b2d686072
jessie36az <jessicaspiess358@gmail.com>
2024-06-12 01:34:18 -0700

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -3,69 +3,66 @@
 PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
 
 echo -e "\n~~~~~ MY SALON ~~~~~\n"
+echo -e "Welcome to My Salon, how can I help you?\n"
 
 MAIN_MENU() {
-  if [[ $1 ]]
-  then
-    echo -e "\n$1. What would you like today?"
-  else
-    echo "Welcome to My Salon, how can I help you?" 
-  fi
-
-  AVAILABLE_SERVICES=$($PSQL "SELECT service_id, name FROM services;")
-  echo "$AVAILABLE_SERVICES" | while read SERVICE_ID BAR NAME
+  echo "Please select a service from the list below:"
+  
+  # Get all services from the database
+  SERVICES_OFFERED=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
+  
+  # Display the services
+  echo "$SERVICES_OFFERED" | while read SERVICE_ID BAR NAME
   do
     echo "$SERVICE_ID) $NAME"
   done
+
   read SERVICE_ID_SELECTED
 
-  if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]
+  # Validate service selection
+  SERVICE_ID_SELECTED_LOOKUP=$($PSQL "SELECT service_id FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+  
+  if [[ -z $SERVICE_ID_SELECTED_LOOKUP ]]
   then
-    # send to main menu
-    MAIN_MENU "That is not a valid service number"
+    echo -e "\nInvalid service selection. Please try again.\n"
+    MAIN_MENU
   else
-    SERVICE_AVAILABILITY=$($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED")
-
-    # if not available
-    if [[ -z $SERVICE_AVAILABILITY ]]
+    echo -e "\nWhat's your phone number?"
+    read CUSTOMER_PHONE
+    
+    CUSTOMER_NAME_LOOKUP=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
+    
+    if [[ -z $CUSTOMER_NAME_LOOKUP ]]
     then
-      # send to main menu
-      MAIN_MENU "I could not find that service"
+      echo -e "\nI don't have a record for that phone number. What's your name?"
+      read CUSTOMER_NAME
+      INSERT_CUSTOMER_INFO=$($PSQL "INSERT INTO customers(phone, name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
     else
-      # get customer info
-      echo -e "\nWhat's your phone number?"
-      read CUSTOMER_PHONE
-
-      CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
-
-      # if customer doesn't exist
-      if [[ -z $CUSTOMER_NAME ]]
-      then
-        # get new customer name
-        echo -e "\nI don't have a record for that phone number, what's your name?"
-        read CUSTOMER_NAME
-
-        # insert new customer
-        INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
-        CUSTOMER_NAME_FORMATTED=$CUSTOMER_NAME
-      else
-        CUSTOMER_NAME_FORMATTED=$(echo $CUSTOMER_NAME | sed 's/^ *//')
-      fi
-      
-      SERVICE_NAME_FORMATTED=$(echo $SERVICE_AVAILABILITY | sed 's/^ *//')
-
-      echo -e "\nWhat time would you like your $SERVICE_NAME_FORMATTED, $CUSTOMER_NAME_FORMATTED?"
-      read SERVICE_TIME
-
-      # get customer_id
-      CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
-
-      # insert appointment
-      INSERT_APPOINTMENT_RESULT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
-      
-      echo -e "\nI have put you down for a $SERVICE_NAME_FORMATTED at $SERVICE_TIME, $CUSTOMER_NAME_FORMATTED."
+      CUSTOMER_NAME=$CUSTOMER_NAME_LOOKUP
     fi
+    
+    SERVICE_TIME
   fi
 }
 
+SERVICE_TIME() {
+  echo -e "\nWhat time would you like your appointment, $CUSTOMER_NAME?"
+  read SERVICE_TIME
+  
+  CUSTOMER_ID_LOOKUP=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+  
+  INSERT_APPOINTMENT_INFO=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID_LOOKUP, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+  
+  SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
+
+  echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
+  EXIT
+}
+
+EXIT() {
+  echo -e "\nThank you for stopping in.\n"
+  exit
+}
+
 MAIN_MENU
+pg_dump -cC --inserts -U freecodecamp salon > salon.sql
